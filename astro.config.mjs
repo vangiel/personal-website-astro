0492632061b77d897c9b99a1a605c4b02e962d47
Daniel Rodriguez <dani1610@hotmail.com>
2024-03-17 13:19:48 +0100

    added three projects

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,26 @@
+import cloudflare from "@astrojs/cloudflare";
 import icon from "astro-icon";
 import { defineConfig } from "astro/config";
 
-import cloudflare from "@astrojs/cloudflare";
+import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [icon()],
+	integrations: [
+		icon(),
+		starlight({
+			title: "Dr Daniel projects",
+			logo: {
+				src: "./src/icons/logo.svg",
+			},
+			// customCss: ['./src/custom-styles.css', '@fontsource/roboto'],
+			social: {
+				"github": "https://github.com/vangiel",
+				"linkedin": "https://www.linkedin.com/in/daniel-rodr%C3%ADguez-criado-phd-6967626a/",
+				"x.com": "https://twitter.com/danirocri",
+			},
+		}),
+	],
 	output: "server",
 	adapter: cloudflare({
 		imageService: "passthrough",
