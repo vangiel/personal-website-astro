dfab609df662bf81bb15ebf54418faf9793a7a58
Daniel Rodriguez <dani1610@hotmail.com>
2024-03-21 14:09:44 +0100

    First form of the blog page

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,6 +1,15 @@
 import { docsSchema } from "@astrojs/starlight/schema";
-import { defineCollection } from "astro:content";
+import { defineCollection, z } from "astro:content";
 
 export const collections = {
 	docs: defineCollection({ schema: docsSchema() }),
+	blog: defineCollection({
+		type: "content",
+		schema: z.object({
+			heroImage: z.string(),
+			title: z.string(),
+			tags: z.string().array(),
+			pubDate: z.coerce.date(),
+		}),
+	}),
 };
