f5dce6ac68403047374771c3960440b15c0bd8f1
Daniel Rodriguez <dani1610@hotmail.com>
2024-05-28 13:27:47 +0200

    First version of contact page finished

---
+++ src/pages/api/recaptcha.ts
@@ -0,0 +1,24 @@
+export const prerender = false;
+
+export async function POST({ request }: { request: Request }) {
+	const data = await request.json();
+
+	const recaptchaURL = "https://www.google.com/recaptcha/api/siteverify";
+	const requestHeaders = {
+		"Content-Type": "application/x-www-form-urlencoded",
+	};
+	const requestBody = new URLSearchParams({
+		secret: import.meta.env.RECAPTCHA_SECRET_KEY, // This can be an environment variable
+		response: data.recaptcha, // The token passed in from the client
+	});
+
+	const response = await fetch(recaptchaURL, {
+		method: "POST",
+		headers: requestHeaders,
+		body: requestBody.toString(),
+	});
+
+	const responseData = await response.json();
+
+	return new Response(JSON.stringify(responseData), { status: 200 });
+}
